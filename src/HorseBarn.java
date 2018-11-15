9f9860737c6dcd816cbc3c0a06e1023861c61cf7
BT_1N3_19@BKTECH.LOCAL <BT_1N3_19@BKTECH.LOCAL>
2018-11-15 14:48:15 -0500

    Initial commit

---
+++ src/HorseBarn.java
@@ -0,0 +1,15 @@
+public class HorseBarn {
+
+    private Horse[] spaces;
+
+    public int findHorseSpace(String name) {
+        for (int i = 0; i < this.spaces.length-1; i++) {
+            if (this.spaces[i] != null && name.equals(this.spaces[i].getName())) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+}
---
+++ src/Mustang.java
@@ -0,0 +1,18 @@
+public class Mustang implements Horse {
+
+    private String name;
+    private double weight;
+
+    public Mustang(String name, double weight) {
+        this.name = name;
+        this.weight = weight;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public double getWeight() {
+        return this.weight;
+    }
+}
